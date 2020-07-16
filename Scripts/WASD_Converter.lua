6355b13266b89bf7ad3f7245b23c5b8c0d15df57
TechnologicNick <technologicnick@gmail.com>
2020-07-16 02:23:19 +0200

    W, A, S and D now work

--- Scripts/WASD_Converter.lua
+++ Scripts/WASD_Converter.lua
@@ -21,20 +21,20 @@ Converter.poseWeightCount = 1
 
 
 
-function Converter.client_onFixedUpdate( self, timeStep )
-    self:updateSeat()
-    
-    -- Check if the player is in the parent seat
-    if self:cl_isInSeat() then
-        
-    end
-    
-    self:cl_updateVisuals()
-end
-
-function Converter.cl_isInSeat( self )
-    return self.seat and sm.localPlayer.getPlayer().character == self.seat:getSeatCharacter()
-end
+--function Converter.client_onFixedUpdate( self, timeStep )
+--    self:updateSeat()
+--    
+--    -- Check if the player is in the parent seat
+--    if self:cl_isInSeat() then
+--        
+--    end
+--    
+--    self:cl_updateVisuals()
+--end
+--
+--function Converter.cl_isInSeat( self )
+--    return self.seat and sm.localPlayer.getPlayer().character == self.seat:getSeatCharacter()
+--end
 
 function Converter.sv_setEnabled( self, enabled, power )
     --print(enabled, power)
@@ -66,6 +66,29 @@ function Converter.updateSeat( self )
     self.seat = self.interactable:getSingleParent()
 end
 
+function Converter.calculateEnabled( self )
+    self:updateSeat()
+    
+    print(self.seat)
+    if self.seat then
+        local value = self.converterFunction(self.seat) * (self.inverted and -1 or 1)
+        
+        --self:sv_setEnabled(value == (self.inverted and 1 or 0), value * (self.inverted and 1 or -1))
+        
+        if value == 1 then -- W
+            self:sv_setEnabled(true, 1)
+        elseif value == -1 then -- S
+            self:sv_setEnabled(false, -1)
+        else
+            self:sv_setEnabled(false, 0.0)
+        end
+    end
+end
+
+function Converter.server_onFixedUpdate( self, timeStep )
+    self:calculateEnabled()
+end
+
 
 
 
@@ -78,6 +101,26 @@ A_Converter = class( Converter )
 S_Converter = class( Converter )
 D_Converter = class( Converter )
 
+function W_Converter.server_onCreate( self )
+    self.converterFunction = self.interactable.getSteeringPower
+end
+
+function A_Converter.server_onCreate( self )
+    self.converterFunction = self.interactable.getSteeringAngle
+    self.inverted = true
+end
+
+function S_Converter.server_onCreate( self )
+    self.converterFunction = self.interactable.getSteeringPower
+    self.inverted = true
+end
+
+function D_Converter.server_onCreate( self )
+    self.converterFunction = self.interactable.getSteeringAngle
+end
+
+
+--[[
 function A_Converter.server_onCreate( self )
     self.parentPoseWeight = 0.5
     self.prevParentPoseWeight = 0.5
@@ -140,3 +183,4 @@ function D_Converter.server_onFixedUpdate( self, timeStep )
         self.prevParentPoseWeight = self.parentPoseWeight
     end
 end
+]]
