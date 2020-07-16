3346678a014a0b9eda462674a25d0b0c3ee8a5fd
TechnologicNick <technologicnick@gmail.com>
2020-07-16 02:24:45 +0200

    Cleanup

--- Scripts/WASD_Converter.lua
+++ Scripts/WASD_Converter.lua
@@ -21,24 +21,7 @@ Converter.poseWeightCount = 1
 
 
 
---function Converter.client_onFixedUpdate( self, timeStep )
---    self:updateSeat()
---    
---    -- Check if the player is in the parent seat
---    if self:cl_isInSeat() then
---        
---    end
---    
---    self:cl_updateVisuals()
---end
---
---function Converter.cl_isInSeat( self )
---    return self.seat and sm.localPlayer.getPlayer().character == self.seat:getSeatCharacter()
---end
-
 function Converter.sv_setEnabled( self, enabled, power )
-    --print(enabled, power)
-
     self.interactable.power = power
     self.interactable.active = enabled
     
@@ -73,11 +56,9 @@ function Converter.calculateEnabled( self )
     if self.seat then
         local value = self.converterFunction(self.seat) * (self.inverted and -1 or 1)
         
-        --self:sv_setEnabled(value == (self.inverted and 1 or 0), value * (self.inverted and 1 or -1))
-        
-        if value == 1 then -- W
+        if value == 1 then
             self:sv_setEnabled(true, 1)
-        elseif value == -1 then -- S
+        elseif value == -1 then
             self:sv_setEnabled(false, -1)
         else
             self:sv_setEnabled(false, 0.0)
@@ -118,69 +99,3 @@ end
 function D_Converter.server_onCreate( self )
     self.converterFunction = self.interactable.getSteeringAngle
 end
-
-
---[[
-function A_Converter.server_onCreate( self )
-    self.parentPoseWeight = 0.5
-    self.prevParentPoseWeight = 0.5
-end
-D_Converter.server_onCreate = A_Converter.server_onCreate
-
-
-function W_Converter.server_onFixedUpdate( self, timeStep )
-    if self.seat then
-        if self.seat:getSteeringPower() > 0 then -- W
-            self:sv_setEnabled(true, 1.0)
-        elseif self.seat:getSteeringPower() < 0 then -- S
-            self:sv_setEnabled(false, -1.0)
-        else
-            self:sv_setEnabled(false, 0.0)
-        end
-    end
-end
-
-function A_Converter.server_onFixedUpdate( self, timeStep )
-    --print(self.seat, self.seat and self.seat:getSteeringAngle())
-
-    if self.seat then
-        if (self.parentPoseWeight < self.prevParentPoseWeight and self.parentPoseWeight < 0.5) or self.parentPoseWeight == 0 then
-            --print("A")
-            self:sv_setEnabled(true, 1.0)
-        elseif (self.parentPoseWeight > self.prevParentPoseWeight and self.parentPoseWeight > 0.5) or self.parentPoseWeight == 1 then
-            --print("D")
-            self:sv_setEnabled(false, -1.0)
-        else
-            self:sv_setEnabled(false, 0.0)
-        end
-        self.prevParentPoseWeight = self.parentPoseWeight
-    end
-end
-
-function S_Converter.server_onFixedUpdate( self, timeStep )
-    if self.seat then
-        if self.seat:getSteeringPower() < 0 then -- S
-            self:sv_setEnabled(true, 1.0)
-        elseif self.seat:getSteeringPower() > 0 then -- W
-            self:sv_setEnabled(false, -1.0)
-        else
-            self:sv_setEnabled(false, 0.0)
-        end
-    end
-end
-
-function D_Converter.server_onFixedUpdate( self, timeStep )
-    if self.seat then
-        if (self.parentPoseWeight > self.prevParentPoseWeight and self.parentPoseWeight > 0.5) or self.parentPoseWeight == 1 then
-            --print("D")
-            self:sv_setEnabled(true, 1.0)
-        elseif (self.parentPoseWeight < self.prevParentPoseWeight and self.parentPoseWeight < 0.5) or self.parentPoseWeight == 0 then
-            --print("A")
-            self:sv_setEnabled(false, -1.0)
-        else
-            self:sv_setEnabled(false, 0.0)
-        end
-        self.prevParentPoseWeight = self.parentPoseWeight
-    end
-end
-]]
